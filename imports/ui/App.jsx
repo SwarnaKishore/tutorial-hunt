5fa46dadba9f4f184f638c824c60b1c7fe28b45e
SwarnaKishore <swarnakishoree@gmail.com>
2021-03-19 15:14:53 -0500

    updated the navbar and tag line

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,7 +6,12 @@ import { NavBar } from './NavBar';
 export const App = () => (
   <div>
     <NavBar/>
-    <AddTutorialForm/>
+    <div className="container">
+      <div className="row">
+        <h5 className="center tag-line">View and share the best and free tutorials at one place</h5>
+      </div>
+    </div>
+    {/* <AddTutorialForm/> */}
     <TutorialList/>
   </div>
 );
--- imports/ui/NavBar.jsx
+++ imports/ui/NavBar.jsx
@@ -4,12 +4,15 @@ export const NavBar = () =>   {
     return (
             <nav>
                 <div class="nav-wrapper">
-                <a class="brand-logo">Tutorial Hunt</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    {/* <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li> */}
-                </ul>
+                    <div class="container"> 
+                        <a class="brand-logo">Tutorial Hunt</a>
+                    </div>
+                
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        {/* <li><a href="sass.html">Sass</a></li>
+                        <li><a href="badges.html">Components</a></li>
+                        <li><a href="collapsible.html">JavaScript</a></li> */}
+                    </ul>
                 </div>
             </nav>
     )
